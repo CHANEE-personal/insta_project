4cb6d5126725891062af2a6ca2ed10c3997d1f31
CHO CHAN HEE <cksgml4424@gmail.com>
2023-08-20 18:20:32 +0900

    cors 관련 설정 변경

--- src/test/java/org/insta_project/config/WebConfiguration.java
+++ src/test/java/org/insta_project/config/WebConfiguration.java
@@ -25,7 +25,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 //				.allowedOrigins("http://ec2-3-36-127-153.ap-northeast-2.compute.amazonaws.com/")
-                .allowedOrigins("http://localhost:8080/")
+                .allowedOrigins("http://localhost:3000/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS").allowedHeaders("*")
 
                 .maxAge(3600);
